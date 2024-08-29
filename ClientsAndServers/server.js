1d8659accb4ab80962494e954010a8615b3d6051
eworsham <eworsham64@gmail.com>
2024-08-29 16:41:33 -0400

    Response object

--- ClientsAndServers/server.js
+++ ClientsAndServers/server.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log('request made')
+    console.log(req.url, req.method)
+
+    // Set header content type
+    res.setHeader('Content-Type', 'text/html')
+
+    res.write('<h1>Hello World</h1>')
+    res.write('<p>hello, again</p>')
+    res.end()
 })
 
 server.listen(3000, 'localhost', () => {
