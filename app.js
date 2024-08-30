fa2c97f502e08827ec0ed7ee3b2535e907a486c2
eworsham <eworsham64@gmail.com>
2024-08-30 15:26:30 -0400

    Express introduction

--- app.js
+++ app.js
@@ -7,5 +7,21 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>hello, world</p>');
+    // res.send('<p>home page</p>');
+    res.sendFile(`${__dirname}/views/index.html`);
+})
+
+app.get('/about', (req, res) => {
+    // res.send('<p>about page</p>');
+    res.sendFile(`${__dirname}/views/about.html`);
+})
+
+// redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+})
+
+// 404 page (must alway be at the bottom of the file)
+app.use((req, res) => {
+    res.status(404).sendFile(`${__dirname}/views/404.html`);
 })
